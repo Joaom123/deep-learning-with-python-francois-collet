a4089b62e7aac08b9906d30a15a7ad1eb5928704
João Marcus <joaomarcus12769@gmail.com>
2021-01-27 01:44:34 -0300

    [update] add the neural network arch and the compilation step

--- chapter2-mathematicalBuildingBlocksOfNeuralNetworks/aFirstLookAtANeuralNetwork.py
+++ chapter2-mathematicalBuildingBlocksOfNeuralNetworks/aFirstLookAtANeuralNetwork.py
@@ -1,3 +1,33 @@
+from keras import models, layers
 from keras.datasets import mnist
 
+# the images and labels have one-to-one correspondence
 (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+
+# see the data we dealing with
+# print(train_images.shape)
+# print(train_labels)  # 0 to 9
+# print(train_labels.shape)
+
+# the network architecture
+network = models.Sequential()
+network.add(
+    layers.Dense(
+        512,
+        activation='relu',
+        input_shape=(28 * 28,)
+    )
+)
+network.add(
+    layers.Dense(
+        10,
+        activation='softmax'
+    )
+)
+
+# the compilation step
+network.compile(
+    optimizer='rmsprop',
+    loss='categorical_crossentropy',
+    metrics=['accuracy']
+)
